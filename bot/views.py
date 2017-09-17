8b5d1852f0e57d40340cc77dd4d0593c60696a4e
leader <leader931334@outlook.jp>
2017-09-18 04:39:28 +0900

    fix os.getenv()

--- bot/views.py
+++ bot/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
-ACCESS_TOKEN = os.environ("LINE_ACCESS_TOKEN")
+ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 HEADER = {
     "Content-Type": "application/json",
     "Authorization": "Bearer " + ACCESS_TOKEN
