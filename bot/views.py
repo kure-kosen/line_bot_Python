ccd7b7444002a06475a8947b1bbd020d683120d3
leader <leader931334@outlook.jp>
2017-09-18 15:44:57 +0900

    fix views.py

--- bot/views.py
+++ bot/views.py
@@ -9,13 +9,13 @@ from django.http import HttpResponse
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN") #セキュリティの観点から環境変数に設定
 HEADER = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + ACCESS_TOKEN
+	"Content-Type": "application/json",
+	"Authorization": "Bearer " + ACCESS_TOKEN
 }
 
 
 def index(request):
-    return HttpResponse("It works!!")
+	return HttpResponse(request)
 
 
 def callback(request_json):
@@ -24,8 +24,8 @@ def callback(request_json):
 	for e in request['events']:
 		reply_token = e['reply_Token']
 
-		if e['type'] == 'message':
-			if e['message']['type'] == 'text':
+		if e['type'] == "message":
+			if e['message']['type'] == "text":
 				reply += make_text()
 			else:
 				reply += "今はテキストのみ返信できます"
@@ -42,14 +42,14 @@ def make_text():
 
 def reply_message(reply_token, reply):
 	reply_body = {
-          'replyToken':reply_token,
-          'messages':[
-                {
-                    'type':"text",
-                    'text': reply
-                }
-            ]
-    }
+        	'replyToken':reply_token,
+        	'messages':[
+                	{
+                    		'type':"text",
+                    		'text': reply
+                	}
+            	]
+    	}
 
 	requests.post(REPLY_ENDPOINT, headers=HEADER, body=json.dumps(reply_body))
 
