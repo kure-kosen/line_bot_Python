b998f744fc43d6c3e12d90e79743d5ae5a816ca4
leader <leader931334@outlook.jp>
2017-10-01 02:46:13 +0900

    fix

--- bot/views.py
+++ bot/views.py
@@ -18,7 +18,7 @@ def index(request):
 
 def callback(request_json_origin):
     reply = ""
-    request_json = request_json_origin.POST[events]
+    request_json = request_json_origin.POST["events"]
     request = json.loads(request_json.body.decode('utf-8'))
     for e in request['events']:
         reply_token = e['replyToken']
